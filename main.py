bb7ed9700a6c9352116255e5488d7887a66689b1
emanuele-f <black.silver@hotmail.it>
2015-09-30 00:44:37 +0200

    detect slow downs

--- main.py
+++ main.py
@@ -50,6 +50,8 @@ class BulletSimu(wx.App):
         self.simulation = Simulation()
         self.state = SIMULATION_STATE_READY
         self.obstacles = []
+        self._oldt = 0
+        self._tickrate = 1./SIMULATION_FPS       # ticks/sec
 
     def OnInit(self):
         self.frame = BulletSimuFrame()
@@ -111,9 +113,11 @@ class BulletSimu(wx.App):
             self.frame.panelParams.Disable()
             self.frame.panelObstacles.Disable()
             self.frame.panelSimulation.SetSimulation(self.simulation)
-            self.timer.Start(1000./SIMULATION_FPS)
+            self.timer.Start(self._tickrate * 1000, oneShot=True)
+            self._oldt = 0
             self.frame.panelStatus.SetStatus("Stop")
             self.state = SIMULATION_STATE_RUNNING
+            print "Will tick every %d ms" % int(self._tickrate * 1000)
         else: # SIMULATION_STATE_RUNNING | SIMULATION_STATE_FINISHED
             self.simulation.Stop(curtime)
             self.frame.panelParams.Enable()
@@ -161,13 +165,22 @@ class BulletSimu(wx.App):
             self.frame.panelStatus.SetStatus("Reset")
             self.state = SIMULATION_STATE_FINISHED
 
-        self.frame.panelSimulation.Refresh()
-        self.frame.panelStatus.UpdateData(
-            self.simulation.x,
-            self.simulation.y,
-            self.simulation.vx,
-            self.simulation.vy,
-            t)
+        # only update graphics if we are not running slow
+        if finished or not self._oldt or (t - self._oldt) < self._tickrate:
+            self.frame.panelSimulation.Refresh()
+            self.frame.panelStatus.UpdateData(
+                self.simulation.x,
+                self.simulation.y,
+                self.simulation.vx,
+                self.simulation.vy,
+                t)
+            
+        if not finished:
+            t = time.time() - self.simulation.t0 - self._oldt
+            # reset new timeout using current value as a reference, with a 10% advance
+            self.timer.Start(max((self._tickrate - t)*1000*0.9, 0), oneShot=True)
+        
+        self._oldt = time.time() - self.simulation.t0
 
     def OnObstaclesChange(self, event):
         self.obstacles = self.frame.panelObstacles.GetObstaclesRects()
