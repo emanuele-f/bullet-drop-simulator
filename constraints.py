dd5c031e2e0c265e1aa59e10cc22d0b4ed778e88
emanuele-f <black.silver@hotmail.it>
2015-09-30 01:40:55 +0200

    make frame skipping a configuration parameter

--- constraints.py
+++ constraints.py
@@ -27,6 +27,8 @@ GROUND_HEIGHT = 50
 
 UNITSIZE = 40
 SIMULATION_FPS = 40
+# Skip some frame visualization when simulation is running slow
+FRAME_SKIP = True
 
 # List of initial obstacles, in the form (x, y, w, h)
 #~ INITIAL_OBSTACLES = ((7,0,2,1), (3,0,3,3))
--- main.py
+++ main.py
@@ -165,17 +165,18 @@ class BulletSimu(wx.App):
             self.frame.panelStatus.SetStatus("Reset")
             self.state = SIMULATION_STATE_FINISHED
 
-        # only update graphics if we are not running slow
-        if finished or not self._oldt or (t - self._oldt) < self._tickrate:
+        # only update heavy graphics if we are not running slow
+        if finished or not FRAME_SKIP or not self._oldt or (t - self._oldt) < self._tickrate:
             self.frame.panelSimulation.UpdateDrawing()
-            self.frame.panelStatus.UpdateData(
-                self.simulation.x,
-                self.simulation.y,
-                self.simulation.vx,
-                self.simulation.vy,
-                t)
         #~ else:
             #~ print "too slow!"
+            
+        self.frame.panelStatus.UpdateData(
+            self.simulation.x,
+            self.simulation.y,
+            self.simulation.vx,
+            self.simulation.vy,
+            t)
         
         self._oldt = time.time() - self.simulation.t0
 
